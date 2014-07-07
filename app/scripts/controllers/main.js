4ed7754e045d5b8ef3cf8a83a417b3f56144c65a
Chris Gunawardena <chris@gunawardena.id.au>
2014-07-08 01:29:03 +1000

    Project management

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,50 @@
 'use strict';
 
 angular.module('jiraTimeLapseApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-  });
+.controller('MainCtrl', function ($scope, $http) {
+
+
+	var load_projects = function(){
+		//Load current projects
+		$http.get('/api/projects').success( function(projects) {
+			$scope.projects = projects;
+		});			
+	};
+	load_projects();
+
+	//Init
+	$scope.current_project = false;
+
+	$scope.save_project = function(){
+		if( $scope.current_project._id )
+		{	$http.post('/api/projects/'+$scope.current_project._id, $scope.current_project).success( function(saved_project) {
+				$scope.current_project = false;
+				load_projects();
+			});
+		}else{
+			$http.post('/api/projects', $scope.current_project).success( function(saved_project) {
+				$scope.current_project = false;
+				load_projects();
+			});					
+		}
+	};
+
+	$scope.add_project = function(){
+		$scope.current_project = { jira_url: 'xxx', jira_username: 'abc', jira_password: '123' };
+	};
+	$scope.open_project = function(project){
+		$scope.current_project = project;
+	};
+	$scope.close_project = function(){
+		$scope.current_project = false;
+	};
+	$scope.delete_project = function(project){
+		if( confirm("Are you sure if you want to delete this project?") )
+		{	$http.delete('/api/projects/'+project._id, $scope.current_project).success( function(saved_project) {
+				$scope.current_project = false;
+				load_projects();
+			});
+		}
+		//return false;
+	};
+});
